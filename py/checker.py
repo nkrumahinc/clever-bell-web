6851fc39f92c02f4e8d7f83e4c78528dfdb40244
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-05-15 20:09:28 +0000

    python: regex check success

--- py/checker.py
+++ py/checker.py
@@ -34,9 +34,13 @@ def is_today(days):
 def is_time(time):
     # check if 24 hour format
 
-    if re.match(r'(1[012]|[1-9]):[0-5][0-9](\s)?(?i)(am|pm)', time):
-        localtime = datetime.now().strftime("%")
+    if re.match(r'^(1[012]|[1-9]):[0-5][0-9](\s)(am|pm|AM|PM)$', time):
+        localtime = datetime.now().strftime("%I:%M %p")
+
+    if re.match(r'^(1[012]|[1-9]):[0-5][0-9](am|pm|AM|PM)$', time):
+        localtime = datetime.now().strftime("%I:%M%p")
     
+
     return (localtime == time)
 
 
@@ -44,12 +48,9 @@ def is_now(row):
     isnow = False
     days = row["days"].capitalize()
     time = row["time"].strip()
-    print(' check isnow' )
+    # print(' check isnow' )
     if is_today(days) and is_time(time):
-        print(" time matches" )
-        isnow = True
+        # print(" time matches" )
+        return TESTMODE or True
 
-    else: 
-        isnow = False
-
-    return TESTMODE or isnow
+    return TESTMODE or False
