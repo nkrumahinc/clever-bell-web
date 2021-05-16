782b3515675728d691a5e2d2f18a83169b5e8600
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-05-16 12:56:35 +0000

    redo: play jingle

--- py/play.py
+++ py/play.py
@@ -1,7 +1,33 @@
+from pygame import mixer
+from pygame import error as sounderror
+
+from audioplayer import AudioPlayer
+
+import time
+from py.say import say
+
+mixer.init()
 
 def initjingles():
     # if jingles folder does not exist, create it
     pass
 
+def playx(filename):
+    jingle = "jingles/"+filename
+
+    try:
+        mixer.music.load(jingle)
+        mixer.music.play()
+        while mixer.music.get_busy():
+            time.sleep(1)
+    
+    except sounderror as errormsg:
+        say(errormsg)
+        print(errormsg)
+
+    # print("plays " + filename)
+
 def play(filename):
-    print("plays " + filename)
+    jingle = "jingles/"+filename
+    player = AudioPlayer(jingle)
+    player.play(loop=False,block=True)
