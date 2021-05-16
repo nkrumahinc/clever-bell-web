876b0b440e634d66e6abcda4fbabd657d1f8b6f3
Nkrumah Sarpong <nkrumahthis@gmail.com>
2021-05-16 16:48:33 +0000

    voice: female voice

--- py/say.py
+++ py/say.py
@@ -1,2 +1,9 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.setProperty('voice', 'english+f4')
+
 def say(text):
-    print("cleverbell says:", text)
+    engine.say(text)
+    engine.runAndWait()
+    # print("cleverbell says:", text)
